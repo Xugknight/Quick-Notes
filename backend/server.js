bd24736f120630f34a5655b9591d6f1b6a8985be
timothyn <tnguyen_42@yahoo.com>
2025-09-03 15:41:46 -0400

    Prep for heroku

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.get('/api/health', (_req, res) => {
 
 app.use('/api/notes', require('./routes/notes'));
 
-app.get('/*splat', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
